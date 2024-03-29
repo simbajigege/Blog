86ff10e1f5a68c9db65eae4fd823f3ebff2dc4b0
simbajigege <jisimba6@gmail.com>
2024-03-29 16:25:45 +0800

    first commit, initialize

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -76,67 +76,66 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Doc',
+            label: 'Tutorial',
           },
-​          {to: '/blog', label: 'Blogee2', position: 'left'},
-​          {to: '/bloge', label: 'Blogee1', position: 'left'},
-​          {
-​            href: 'https://github.com/facebook/docusaurus',
-​            label: 'GitHub',
-​            position: 'right',
-​          },
-​        ],
-​      },
-​      footer: {
-​        style: 'dark',
-​        links: [
-​          {
-​            title: 'Docs',
-​            items: [
-​              {
-​                label: 'Tutorial',
-​                to: '/docs/intro',
-​              },
-​            ],
-​          },
-​          {
-​            title: 'Community',
-​            items: [
-​              {
-​                label: 'Stack Overflow',
-​                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-​              },
-​              {
-​                label: 'Discord',
-​                href: 'https://discordapp.com/invite/docusaurus',
-​              },
-​              {
-​                label: 'Twitter',
-​                href: 'https://twitter.com/docusaurus',
-​              },
-​            ],
-​          },
-​          {
-​            title: 'More',
-​            items: [
-​              {
-​                label: 'Blog',
-​                to: '/blog',
-​              },
-​              {
-​                label: 'GitHub',
-​                href: 'https://github.com/facebook/docusaurus',
-​              },
-​            ],
-​          },
-​        ],
-​        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-​      },
-​      prism: {
-​        theme: prismThemes.github,
-​        darkTheme: prismThemes.dracula,
-​      },
-​    }),
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
 };
 
 export default config;
